cdaf705dc172ac5dd555982f9790fd8cb7951192
sujit <sujit.shrc@gmail.com>
2024-08-11 22:12:41 +0530

    add and customize neotree i.e file explorer

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "left",
+      width = 22, -- Set the width here (e.g., 30 columns)
+      mappings = {
+        ["Y"] = "none",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+        },
+        always_show = {
+          ".env",
+        },
+      },
+    },
+  },
+}
